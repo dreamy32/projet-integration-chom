5da458c42de613e3bce6a860d936492b33a29192
Dr. Seek <75817900+dreamy32@users.noreply.github.com>
2023-12-12 16:44:54 -0500

    Update InteractionManager.cs

--- Interact-Terminal/InteractionManager.cs
+++ Interact-Terminal/InteractionManager.cs
@@ -11,10 +11,5 @@ public class InteractionManager : MonoBehaviour
     public static event InteractAction OnInteract;
     
     //Event
-    // public void Interact(InputAction.CallbackContext ctx)
-    // {
-    //     if (ctx.performed)
-    //         OnInteract?.Invoke();
-    // }
     public static void Interact() => OnInteract?.Invoke();
 }
